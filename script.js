381d370bd57a257c1607a426e48593ab5dfd3306
Andrey Roshchupkin <reivrn@gmail.com>
2024-05-07 17:18:32 +0400

    Time updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Specify your target date in the format: "Month Day, Year HH:mm:ss"
-  var targetDate = new Date("April 27, 2024 16:45:00").getTime();
+  var targetDate = new Date("May 9, 2024 10:00:00").getTime();
 
   var countdownElement = document.getElementById("countdown");
 
