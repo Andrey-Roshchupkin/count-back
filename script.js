0550f4f97b3555a09ac150dad5e27846ea612957
Andrew Roshchupkin <sdet.andrew@gmail.com>
2024-09-16 09:22:51 +0400

    time was updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Specify your target date in the format: "Month Day, Year HH:mm:ss"
-  var targetDate = new Date("June 22, 2024 16:45:00").getTime();
+  var targetDate = new Date("October 2, 2024 04:05:00").getTime();
 
   var countdownElement = document.getElementById("countdown");
 
@@ -11,11 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (timeDifference > 0) {
       var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+      var hours = Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor(
+        (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+      );
       var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-      countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+      countdownElement.innerHTML =
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s";
     } else {
       clearInterval(countdownInterval);
       countdownElement.innerHTML = "Already arrived";
